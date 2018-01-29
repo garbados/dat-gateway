9366f5f940437388112acb2125486273378a7999
Diana Thayer <garbados@gmail.com>
2018-01-29 11:25:25 -0800

    begin testing

---
+++ test.js
@@ -0,0 +1,10 @@
+/* global describe it */
+
+const assert = require('assert')
+const DatGateway = require('.')
+
+describe('dat-gateway', function () {
+  it('should exist', function () {
+    assert(DatGateway)
+  })
+})
