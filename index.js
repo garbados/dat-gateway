21d3fd4c1747677177048cb4774914d8d2ad77d3
Sebastian Silva <sebastian@fuentelibre.org>
2018-05-06 00:13:54 -0500

    Allow to set the network interface (ip) to listen.

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ class DatGateway {
     })
   }
 
-  listen (port) {
+  listen (port, host) {
     return new Promise((resolve, reject) => {
-      this.server.listen(port, (err) => {
+      this.server.listen(port, host, (err) => {
         if (err) return reject(err)
         else return resolve()
       })
