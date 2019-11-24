e92dfb35a015471914041d628598dd35831758e9
Diana Thayer <garbados@gmail.com>
2019-11-24 15:25:37 -0800

    feat(testing): mock outgoing HTTP requests

--- test.js
+++ test.js
@@ -1,35 +1,95 @@
-/* global describe it before after */
+/* global describe it before after beforeEach afterEach */
 
 const assert = require('assert')
+const fs = require('fs')
 const http = require('http')
-const DatGateway = require('.')
-const rimraf = require('rimraf')
 const hyperdrive = require('hyperdrive')
+const mkdirp = require('mkdirp')
+const nock = require('nock')
+const path = require('path')
 const ram = require('random-access-memory')
+const rimraf = require('rimraf')
 const websocket = require('websocket-stream')
 
-const dir = 'fixtures'
+const DatGateway = require('.')
+
+const NOCK_DIR = '.nock'
+const RECORD_TESTS = !!process.env.RECORD_TESTS
+
+const dir = '.NOCK_DIR'
 const ttl = 4000
 const period = 1000
+
+const recordOrLoadNocks = function () {
+  const titles = []
+  let test = this.currentTest
+  while (test.parent && test.title !== 'dat-gateway') {
+    titles.unshift(test.title)
+    if (test.parent) { test = test.parent }
+  }
+  const dir = path.join(NOCK_DIR, ...titles.slice(0, -1))
+  const name = `${titles.slice(-1)[0]}.json`
+  this._currentNock = { titles, dir, name }
+  if (RECORD_TESTS) {
+    nock.recorder.rec({
+      output_objects: true,
+      dont_print: true
+    })
+  } else {
+    try {
+      nock.load(path.join(dir, name))
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        // no nock
+      } else {
+        throw error
+      }
+    }
+  }
+}
+
+const concludeNocks = function () {
+  if (RECORD_TESTS) {
+    // save http requests for future nocking
+    const { dir, name } = this._currentNock
+    const fixturePath = path.join(dir, name)
+    const nockCallObjects = nock.recorder.play()
+    mkdirp.sync(dir)
+    fs.writeFileSync(fixturePath, JSON.stringify(nockCallObjects), 'utf8')
+    nock.restore()
+    nock.recorder.clear()
+  } else if (!nock.isDone()) {
+    console.error(nock.pendingMocks())
+    throw new Error(`${nock.pendingMocks().length} pending mocks`)
+  }
+}
 
 describe('dat-gateway', function () {
   this.timeout(0)
 
-  before(function () {
-    this.gateway = new DatGateway({dir, ttl, period})
-    return this.gateway.load().then(() => {
-      return this.gateway.listen(5917)
-    })
+  before(async function () {
+    nock.disableNetConnect()
+    nock.enableNetConnect('localhost:5917')
+    this.gateway = new DatGateway({ dir, ttl, period })
+    await this.gateway.load()
+    return this.gateway.listen(5917)
   })
 
-  after(function () {
-    return this.gateway.close().then(() => {
-      rimraf.sync(dir)
-    })
+  after(async function () {
+    await this.gateway.close()
+    rimraf.sync(dir)
+  })
+
+  beforeEach(function () {
+    recordOrLoadNocks.call(this)
+  })
+
+  afterEach(async function () {
+    concludeNocks.call(this)
   })
 
   it('should exist', function () {
-    assert.equal(this.gateway.dir, dir)
+    assert.strictEqual(this.gateway.dir, dir)
   })
 
   it('should handle requests', function () {
@@ -38,7 +98,7 @@ describe('dat-gateway', function () {
       req.on('error', console.log)
     }).then((res) => {
       // should display empty index, s.t. an attacker cannot determine
-      assert.equal(res.statusCode, 200)
+      assert.strictEqual(res.statusCode, 200)
     }).catch((e) => {
       console.error(e)
       throw e
@@ -50,7 +110,7 @@ describe('dat-gateway', function () {
       http.get('http://localhost:5917/af75142d92dd1e456cf2a7e58a37f891fe42a1e49ce2a5a7859de938e38f4642/', resolve)
     }).then((res) => {
       // show blank index
-      assert.equal(res.statusCode, 200)
+      assert.strictEqual(res.statusCode, 200)
     }).catch((e) => {
       console.error(e)
       throw e
@@ -62,7 +122,7 @@ describe('dat-gateway', function () {
       http.get('http://localhost:5917/af75142d92dd1e456cf2a7e58a37f891fe42a1e49ce2a5a7859de938e38f4642', resolve)
     }).then((res) => {
       // show blank index
-      assert.equal(res.statusCode, 302)
+      assert.strictEqual(res.statusCode, 302)
     }).catch((e) => {
       console.error(e)
       throw e
